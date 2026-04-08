8494177a466339f20b7a3803fe52c9554a55889e
Matheus Carvalho <matheus.carvalho.corp@gmail.com>
2026-04-08 17:20:22 +0900

    feat: Included method for insert history dinamically. Refactor calculate and clear display bar for individual methods. Fix: Redesign how values and operators are shown in the display bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,57 @@
 const displayBar = document.getElementById("display");
 const buttons = document.querySelectorAll(".button");
+const historyList = document.getElementById("history-list");
 let valueA = 0;
 let valueB = 0;
 let operator = "";
+let display = "";
 
 buttons.forEach(function(pressedButton) {
     pressedButton.addEventListener("click", function(event) {        
         if (pressedButton.value == "=" && displayBar.value == "") {
             displayBar.value = "Erro!";
         } else if (pressedButton.value != "=") {
-            displayBar.value += pressedButton.value;
+            if(pressedButton.value.match(/[\+\-\*\/]/)) {
+                displayBar.value += ` ${pressedButton.value} `;
+            }
+            else {
+                displayBar.value += pressedButton.value;
+            }
         } else {
-            let display = displayBar.value;
+            display = displayBar.value;
             let values = display.split(/[\+\-\*\/]/);
-            let operators = display.match(/[\+\-\*\/]/g)
+            let operators = display.trim().match(/[\+\-\*\/]/g)
 
             valueA = parseInt(values[0]);
             valueB = parseInt(values[1]);
             operator = operators[0];
 
-            calculatorApi.calculate(valueA, operator, valueB)
-                .then(result => displayBar.value = result);
+            calculate(valueA, operator, valueB);            
         }
 
         if (pressedButton.value == "clear") {
-            displayBar.value = "";
+            clearDisplayBar();
         }        
     })
 })
+
+async function calculate(valueA, operator, valueB) {
+    // debugger
+    await calculatorApi.calculate(valueA, operator, valueB)
+        .then(result => displayBar.value = result);
+
+    insertHistory(display, displayBar.value);
+}
+
+function clearDisplayBar() {
+    displayBar.value = "";
+}
+
+function insertHistory(display, result) {
+    historyList.innerHTML += 
+    `<li>
+        <span>${display} =</span>
+        <p>${result}</p>
+        <hr>
+    </li>`
+}
